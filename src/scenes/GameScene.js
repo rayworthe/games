fa5a0f41d23d3d129e3d76fd5454f3e257d204cd
Eben Rayworth <necro-mancer@hotmail.co.uk>
2019-05-24 07:39:46 +0000

    test

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,82 @@
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super({key : "GameScene"});
+    };
+
+    // Where we preload our images, gifs, sound file and anything else we want to load when the game first opens.
+    preload() {
+         this.load.image("honk", "assets/honkhonksnippet.png");
+    };
+
+    // Created things when the game is running.
+    create() {
+        // Character movement
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.honk = this.physics.add.sprite(50, 50, "honk");
+        this.honk.setScale(0.1);
+        this.honk.setCollideWorldBounds(true);
+
+        this.doublePresses = [];
+
+        this.playerBaseSpeed = 300;
+
+        var arrowKeys = {
+            "left" : this.cursors.left,
+            "up" : this.cursors.up,
+            "right" : this.cursors.right,
+            "down" : this.cursors.down
+        };
+
+        // Used for speeding up the game character
+        const arrowKeyVals = Object.values(arrowKeys);
+        for (const key of arrowKeyVals) {
+            this.doublePress(key, 500, () => {
+                this.playerBaseSpeed = 600;
+            }, () => {
+                this.playerBaseSpeed = 300;
+            });
+        }
+
+
+        // Maze array creation
+        this.generator = require('generate-maze');
+
+        let maze = this.generator(4);
+        console.log(maze);
+    };
+
+    update() {
+        this.honk.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.honk.setVelocityX(this.playerBaseSpeed * -1);
+        } else if (this.cursors.right.isDown) {
+            this.honk.setVelocityX(this.playerBaseSpeed);
+        }
+
+
+        if (this.cursors.up.isDown) {
+            this.honk.setVelocityY(this.playerBaseSpeed * -1);
+        } else if (this.cursors.down.isDown) {
+            this.honk.setVelocityY(this.playerBaseSpeed);
+        }
+    };
+
+    doublePress(cursorKey, delay, pressCallback, resetCallback) {
+        cursorKey.on("down", (event) => {
+            let now = new Date().getTime();
+            if (this.doublePresses[cursorKey.keyCode]) {
+                let difference = now - this.doublePresses[cursorKey.keyCode];
+                if (difference < delay) {
+                    pressCallback();
+                }
+            }
+
+            this.doublePresses[cursorKey.keyCode] = now;
+        });
+        cursorKey.on("up", () => {
+            resetCallback();
+        });
+    }
+}
